ca83376954a55e2caaa19d5e25677c8968ce8309
sinminseok12@naver.com <55183314+sinminseok@users.noreply.github.com>
2025-03-13 21:46:05 +0900

    commit!

---
+++ src/test/java/com/pet/pet_funeral/PetFuneralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.pet_funeral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFuneralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
